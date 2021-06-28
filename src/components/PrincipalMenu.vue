e58f7c15ec3dd982ea2a4aeaacbe8c6645d165b8
Patricio <pflood@frba.utn.edu.ar>
2021-06-27 23:46:56 -0300

    Transition and change menu

--- src/components/PrincipalMenu.vue
+++ src/components/PrincipalMenu.vue
@@ -1,18 +1,20 @@
 <template>
     <div>
-        <div v-show="show" class="menu">  
-            <ul v-show="$route.name != 'home'">
-                <li @click="pushHome"><span class="material-icons" style="color: #2DACA1">home</span>Inicio</li>
-                <li @click="openView('symbolTable')" v-show="$route.name != 'symbolTable'"><span class="material-icons" style="color: #2DACA1">backup_table</span>Tabla de Símbolos</li>
-                <li @click="openView('ladder')" v-show="$route.name != 'ladder'"><span class="material-icons" style="color: #2DACA1">description</span>Bloque de Programa</li>
-                <li @click="openPanelRun()" v-show="!run"><span class="material-icons" style="color: #6BB464">play_arrow</span>Simulación</li>
-                <li @click="resetNetworks()" v-show="$route.name == 'ladder'"><span class="material-icons" style="color: #A03C3C">delete</span>Vaciar Networks</li>
-                <li @click="generateFile()"><span class="material-icons" style="color: #428DD4">download</span>Descargar Archivo</li>
-            </ul>
-            <ul v-show="$route.name == 'home'" class="homeMenu">
-                <li><span class="material-icons" style="color: #2DACA1">login</span>Iniciar Sesión</li>
-            </ul>
-        </div>
+        <transition>
+            <div v-show="show" class="menu">  
+                <ul v-show="$route.name != 'home'">
+                    <li @click="pushHome"><span class="material-icons" style="color: #2DACA1">home</span>Inicio</li>
+                    <li @click="openView('symbolTable')" v-show="$route.name != 'symbolTable'"><span class="material-icons" style="color: #2DACA1">backup_table</span>Tabla de Símbolos</li>
+                    <li @click="openView('ladder')" v-show="$route.name != 'ladder'"><span class="material-icons" style="color: #2DACA1">description</span>Bloque de Programa</li>
+                    <li @click="openPanelRun()" v-show="!run"><span class="material-icons" style="color: #6BB464">play_arrow</span>Simulación</li>
+                    <li @click="resetNetworks()" v-show="$route.name == 'ladder'"><span class="material-icons" style="color: #A03C3C">delete</span>Vaciar Networks</li>
+                    <li @click="generateFile()"><span class="material-icons" style="color: #428DD4">download</span>Descargar Archivo</li>
+                </ul>
+                <ul v-show="$route.name == 'home'">
+                    <li><span class="material-icons" style="color: #2DACA1">login</span>Iniciar Sesión</li>
+                </ul>
+            </div>
+        </transition>
         <alert v-if="alert" @close="alert = false" @check="resetNetworks(true)">¿Desea vaciar todos los networks?</alert>
     </div>
 </template>
@@ -65,22 +67,48 @@ export default {
 }
 </script>
 <style scoped>
+
 .menu{
-    background: rgb(24, 99, 99);
+    background-color: rgb(24, 99, 99);
+    background: linear-gradient(to bottom, rgb(24, 99, 99) -20%, rgb(18, 71, 71) 100%);
     width: 200px;
+    
 }
-.homeMenu{
-    padding-top: 50px;
+
+
+.left-menu .menu{
+    width: 150px;
+    overflow: hidden;
 }
 .lateral-menu .menu{
     height: calc(var(--vh) - 50px);
     right: 0;
     top: 50px;
-    position: absolute;
+    position: fixed;
     z-index: 10;
 }
 .left-menu .menu{
     height: var(--vh);
+}
+
+.lateral-menu .v-enter-to.menu{
+    right: 0;
+}
+.v-enter-active.menu{
+    position: fixed;
+    transition: all 0.2s;
+    right: -200px;
+}
+.lateral-menu .v-leave-to.menu{
+    right: -200px;
+}
+.v-enter-active.menu{
+    transition: all 0.2s;
+    right: -200px;
+}
+.v-leave-active{
+    transition: all 0.2s;
+    right: 0;
 }
 
 li{
@@ -93,7 +121,8 @@ li{
     color: rgb(202, 202, 202);
     font-size: 20px;
     user-select: none;
-    cursor: pointer
+    cursor: pointer;
+    border-bottom: 2px solid rgba(0, 0, 0, 0.1);
 }
 li:hover{
     background: rgb(8, 88, 88);
@@ -102,5 +131,17 @@ li:hover{
 li span{
     font-size: 30px;
     margin-right: 4px;
+}
+
+.left-menu li{
+    flex-direction: column;
+    padding: 10px;
+    text-align: center;
+    font-size: 17px;
+    color: rgba(199, 214, 212, 0.897);
+}
+.left-menu li span{
+    font-size: 50px;
+    margin-bottom: 5px;
 }
 </style>

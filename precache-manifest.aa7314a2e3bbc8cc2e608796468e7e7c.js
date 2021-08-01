7b1726ce0e86b1de3527ed3ca37985a497465f2e
Patricio <pflood@frba.utn.edu.ar>
2021-08-01 15:05:59 -0300

    'deploy'

---
+++ precache-manifest.aa7314a2e3bbc8cc2e608796468e7e7c.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f872237c473e09bc1433182ee6ebb9",
+    "url": "/404.html"
+  },
+  {
+    "revision": "8dba7fb200ddcb8a57e4",
+    "url": "/css/app.6aedd20a.css"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "653a9c320a71c0dc4e6becbf7925e652",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "aeaa1a44fae3fcb6bf67cb2cb42e02d8",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "653a9c320a71c0dc4e6becbf7925e652",
+    "url": "/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "aeaa1a44fae3fcb6bf67cb2cb42e02d8",
+    "url": "/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "1266b4c17c01ddb2aa7b0f54d573b4be",
+    "url": "/img/ladder.1266b4c1.png"
+  },
+  {
+    "revision": "e7d0ca36a8d2af63d17993cebb70049a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dba7fb200ddcb8a57e4",
+    "url": "/js/app.b1ea9910.js"
+  },
+  {
+    "revision": "217c4ce1a586bba4b809",
+    "url": "/js/chunk-vendors.8ecc04f7.js"
+  },
+  {
+    "revision": "e16bb5f37c626b4f664982fd71a62b39",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e3c8555e63db1c2569ea365837decfc8",
+    "url": "/styles/fonts/ladder.eot"
+  },
+  {
+    "revision": "0556113657ccc5f38e887d51ca3be5c7",
+    "url": "/styles/fonts/ladder.svg"
+  },
+  {
+    "revision": "847acfc0a10ae288abf605a9eda8128b",
+    "url": "/styles/fonts/ladder.ttf"
+  },
+  {
+    "revision": "0849305c7e2248350adafa91cf9ba6ab",
+    "url": "/styles/fonts/ladder.woff"
+  },
+  {
+    "revision": "36d5f982e5afe26e60e493ffb1a8f0b7",
+    "url": "/styles/ladder-icons.css"
+  },
+  {
+    "revision": "f3a2153a2dc2825037a5170a581172c7",
+    "url": "/styles/normalice.css"
+  },
+  {
+    "revision": "2155ae43e6516d62de7a9aa780385c45",
+    "url": "/styles/style.css"
+  }
+]);
